a2c075187bbbf5dccd6888adad45786fe5d90759
Banjos <brunosilveiradosanjos@gmail.com>
2025-06-22 16:13:30 -0300

    feat: implement card component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Button from './components/button'
 import ButtonIcon from './components/button-icon'
 import InputText from './components/input-text'
 import InputCheckbox from './components/input-checkbox'
+import Card from './components/card'
 
 export default function App() {
   return (
@@ -50,6 +51,9 @@ export default function App() {
       </div>
       <div>
         <InputCheckbox />
+      </div>
+      <div>
+        <Card size="md">Hello World</Card>
       </div>
     </div>
   )
