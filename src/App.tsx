ee7921a84fec3afbd1708c5186fe5115f9c5513a
Banjos <brunosilveiradosanjos@gmail.com>
2025-06-22 15:26:12 -0300

    feat: implement input-checkbox component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Badge from './components/badge'
 import Button from './components/button'
 import ButtonIcon from './components/button-icon'
 import InputText from './components/input-text'
+import InputCheckbox from './components/input-checkbox'
 
 export default function App() {
   return (
@@ -46,6 +47,9 @@ export default function App() {
       </div>
       <div>
         <InputText placeholder="type your password" />
+      </div>
+      <div>
+        <InputCheckbox />
       </div>
     </div>
   )
